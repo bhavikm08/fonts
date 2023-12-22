4437942e8f08e3c64468a8d039a7721f7495d034
elsner <elsner@elsners-Mac-mini.local>
2023-12-22 15:12:51 +0530

    first commite

---
+++ lib/fonts_constant/fonts_constant.dart
@@ -0,0 +1,7 @@
+class FontConstant{
+  static const String lato = "Lato";
+  static const String poppins = "Poppins";
+  static const String rubikScribble = "Rubik Scribble";
+
+
+}
---
+++ lib/home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:fonts/fonts_constant/fonts_constant.dart';
+import 'package:google_fonts/google_fonts.dart';
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  SafeArea(
+      child: Scaffold(
+        body: Column(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+              Text("Home Page Poppins",style: GoogleFonts.lato(
+                fontWeight: FontWeight.w600,
+                fontSize: 30,
+                color: Colors.blue,
+
+              )),
+              Text("Home Page Poppins",style: GoogleFonts.aboreto(
+                fontWeight: FontWeight.w200,
+                fontSize: 30,
+              )),
+              const Text("Home Page Lato",style: TextStyle(
+              fontSize: 30,
+                fontFamily: FontConstant.lato,
+              )),
+              const Text("Home Page Rubik Scribble",style: TextStyle(
+              fontSize: 30,
+                fontFamily: FontConstant.rubikScribble
+              )),
+          ],
+        ),
+      ),
+    );
+  }
+}
