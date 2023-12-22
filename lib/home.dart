7788f5c587de658c6b7e6f85af4bf803f518002a
elsner <elsner@elsners-Mac-mini.local>
2023-12-22 17:27:25 +0530

    after changes in home dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fonts/fonts_constant/fonts_constant.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -11,29 +12,30 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return  SafeArea(
+    return SafeArea(
       child: Scaffold(
         body: Column(
           // crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-              Text("Home Page Poppins",style: GoogleFonts.lato(
-                fontWeight: FontWeight.w600,
-                fontSize: 30,
-                color: Colors.blue,
-
-              )),
-              Text("Home Page Poppins",style: GoogleFonts.aboreto(
-                fontWeight: FontWeight.w200,
-                fontSize: 30,
-              )),
-              const Text("Home Page Lato",style: TextStyle(
-              fontSize: 30,
-                fontFamily: FontConstant.lato,
-              )),
-              const Text("Home Page Rubik Scribble",style: TextStyle(
-              fontSize: 30,
-                fontFamily: FontConstant.rubikScribble
-              )),
+            Text("Home Page Poppins",
+                style: GoogleFonts.lato(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 30,
+                  color: Colors.blue,
+                )),
+            Text("Home Page Poppins",
+                style: GoogleFonts.aboreto(
+                  fontWeight: FontWeight.w200,
+                  fontSize: 30,
+                )),
+            const Text("Home Page Lato",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontFamily: FontConstant.lato,
+                )),
+            const Text("Home Page Rubik Scribble",
+                style: TextStyle(
+                    fontSize: 31, fontFamily: FontConstant.rubikScribble)),
           ],
         ),
       ),
